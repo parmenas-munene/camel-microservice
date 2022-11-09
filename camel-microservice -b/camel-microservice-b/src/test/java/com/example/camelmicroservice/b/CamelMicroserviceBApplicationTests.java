029895d0e0c9722acb2185949f8d0ac2bc0f767c
Parmenas Munene <parmenasmunene165@gmail.com>
2022-11-10 00:39:38 +0300

    life

---
+++ camel-microservice -b/camel-microservice-b/src/test/java/com/example/camelmicroservice/b/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camelmicroservice.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
